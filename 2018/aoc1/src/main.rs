c1482a5c58397ad1212b3bd258f63b98f63b3413
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2018-12-05 12:30:00 +0530

    Refactor match num.parse()

--- 2018/aoc1/src/main.rs
+++ 2018/aoc1/src/main.rs
@@ -4,10 +4,7 @@ fn main() {
     let data = fs::read_to_string("input.txt").unwrap();
     let mut frequency = 0;
     for num in data.lines() {
-        let change: i32  = match num.parse() {
-            Ok(n) => n,
-            Err(e) => 0
-        };
+        let change: i32  = num.parse().unwrap_or(0);
         frequency += change;
     }
 
