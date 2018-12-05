4572eaabd1846cfdc11045dc07e0b39d5145bdb3
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2018-12-05 12:48:08 +0530

    Update the solution to use accumulator

--- 2018/aoc1/src/main.rs
+++ 2018/aoc1/src/main.rs
@@ -2,11 +2,8 @@ use std::fs;
 
 fn main() {
     let data = fs::read_to_string("input.txt").unwrap();
-    let mut frequency = 0;
-    for num in data.lines() {
-        let change: i32  = num.parse().unwrap_or(0);
-        frequency += change;
-    }
-
+    let frequency = data
+        .lines()
+        .fold(0, |acc, x| x.parse().unwrap_or(0) + acc);
     println!("Frequency: {}", frequency);
 }
