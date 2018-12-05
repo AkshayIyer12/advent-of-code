88905057c4326058bf62e0473ef2a51257a74d02
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2018-12-05 21:10:43 +0530

    Add part2 for problem1

--- 2018/aoc1/src/main.rs
+++ 2018/aoc1/src/main.rs
@@ -1,12 +1,30 @@
 use std::fs;
+use std::collections::HashSet;
 
 fn main() {
     let data = fs::read_to_string("input.txt").unwrap();
-    println!("Frequency: {}", final_frequency(data));
+    println!("Final Frequency: {}", final_frequency(&data));
+    println!("First_frequency: {}", first_frequency(&data));
 }
 
-fn final_frequency(data: String) -> i32 {
+fn final_frequency(data: &String) -> i32 {
     data
         .lines()
         .fold(0, |acc, x| x.parse().unwrap_or(0) + acc)
+}
+
+fn first_frequency(data: &String) -> i32 {
+    let mut freq: i32 = 0;
+    let mut frequencies_observed = HashSet::new();
+    frequencies_observed.insert(0);
+    loop {
+        for line in data.lines() {
+            let change: i32 = line.parse().expect("Error in parsing string to integer");
+            freq += change;
+            if frequencies_observed.contains(&freq) {
+                return freq
+            }
+            frequencies_observed.insert(freq);
+        }   
+    }
 }
