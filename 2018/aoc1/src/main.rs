53aa32c2bf3bdff0c60c51edb9f6203619c9de10
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2018-12-05 13:35:10 +0530

    Move final_frequency calculator to a different function

--- 2018/aoc1/src/main.rs
+++ 2018/aoc1/src/main.rs
@@ -2,8 +2,11 @@ use std::fs;
 
 fn main() {
     let data = fs::read_to_string("input.txt").unwrap();
-    let frequency = data
+    println!("Frequency: {}", final_frequency(data));
+}
+
+fn final_frequency(data: String) -> i32 {
+    data
         .lines()
-        .fold(0, |acc, x| x.parse().unwrap_or(0) + acc);
-    println!("Frequency: {}", frequency);
+        .fold(0, |acc, x| x.parse().unwrap_or(0) + acc)
 }
