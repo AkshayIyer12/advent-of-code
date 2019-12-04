2385807e23bd204281110d639556221226981a78
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2019-12-04 11:01:16 +0530

    Add gdb debug prof & fix aoc2.1

--- 2019/aoc2/src/main.rs
+++ 2019/aoc2/src/main.rs
@@ -4,9 +4,11 @@ fn parse_input() -> Vec<u32> {
     fs::read_to_string("input.txt").unwrap().trim().split(",").map(|c| c.parse().unwrap()).collect()
 }
 
-fn fetch_values_from_vector(mem: &Vec<u32>, c: usize) -> (u32, u32, u32) {
-    let (p1, p2, p3) = (mem[c+1], mem[c+2], mem[c+3]);
-    (mem[p1 as usize], mem[p2 as usize], mem[p3 as usize])
+fn fetch_values_from_vector(mem: &Vec<u32>, c: usize) -> (u32, u32) {
+    let (p1, p2) = (mem[c+1], mem[c+2]);
+    let first: usize = p1 as usize;
+    let second: usize = p2 as usize;
+    (mem[first], mem[second])
 }
 
 fn compute_part1(mut arr2: Vec<u32>) -> Vec<u32> {
@@ -15,12 +17,14 @@ fn compute_part1(mut arr2: Vec<u32>) -> Vec<u32> {
         let opcode = arr2[counter];
         match opcode {
             1 => {
-                let (p1, p2, p3) = fetch_values_from_vector(&arr2, counter);
-                arr2[p3 as usize] = p1 + p2;
+                let (p1, p2) = fetch_values_from_vector(&arr2, counter);
+                let target: u32 = arr2[counter+3];
+                arr2[target as usize] = p1 + p2;
             },
             2 => {
-                let (p1, p2, p3) = fetch_values_from_vector(&arr2, counter);
-                arr2[p3 as usize] = p1 * p2;
+                let (p1, p2) = fetch_values_from_vector(&arr2, counter);
+                let target: u32 = arr2[counter+3];
+                arr2[target as usize] = p1 * p2;
             },
             99 => {
                 break;
