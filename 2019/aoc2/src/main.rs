05282353660999ae9101e219390b7bdab747a730
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2019-12-05 19:57:39 +0530

    Aoc2.2 done

--- 2019/aoc2/src/main.rs
+++ 2019/aoc2/src/main.rs
@@ -36,9 +36,26 @@ fn compute_part1(mut arr2: Vec<u32>) -> Vec<u32> {
     arr2
 }
 
+fn compute_part2(arr: Vec<u32>) -> u32 {
+    for noun in 0..99 {
+        for verb in 0..99 {
+            let mut arr2: Vec<u32> = arr.clone();
+            arr2[1] = noun;
+            arr2[2] = verb;
+            let result = compute_part1(arr2);
+            if result[0] == 19690720 as u32 {
+                return 100 * noun + verb;
+            }
+        }
+    }
+    0
+}
+
+
 fn main() {
     let arr: Vec<u32> = parse_input();
 
     println!("Value at 0th position is: {}", compute_part1(arr.clone())[0]);
+    println!("Part2 answer is: {}", compute_part2(arr));
     
 }
